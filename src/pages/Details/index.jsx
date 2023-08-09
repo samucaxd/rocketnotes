ad377e5887e758a04322f074ab1fa97567210ca1
samuel <samuel.psanttos@gmail.com>
2023-08-09 18:59:11 -0300

    add CSS-in-JS

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,12 @@
-import './styles.css'
+import { Container } from "./styles"
 
 export function Details() {
   
   return (
-    <>
+    <Container>
       <h1>Hello World!</h1>
       <p>Samuel P. Santos</p>
-    </>
+    </Container>
   )
 
 }
