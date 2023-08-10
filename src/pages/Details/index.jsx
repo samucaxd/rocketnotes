02495868cacf9f3c54fea6b1b8fff999770ab0ab
samuel <samuel.psanttos@gmail.com>
2023-08-09 21:27:35 -0300

    styled-components

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,16 @@
 import { Container } from "./styles"
+import { Button } from "../../components/Button"
 
 export function Details() {
-  
+
   return (
     <Container>
       <h1>Hello World!</h1>
       <p>Samuel P. Santos</p>
+
+      <Button name="Entrar" loading/>
+      <Button name="Cadastrar"/>
+      <Button name="Voltar"/>
     </Container>
   )
-
 }
