a5722d024c32a46b3f5be18a262f927d0ae11344
samuel <samuel.psanttos@gmail.com>
2023-08-21 18:43:02 -0300

    Finish Details and new page 'Home'

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,26 @@
+import { Container, Profile, Logout } from './styles'
+import { RiShutDownLine } from 'react-icons/ri'
+
+export function Header() {
+  return (
+    <Container>
+      <Profile>
+        <img 
+          src="https://github.com/samucaxd.png"
+          alt="Foto do usuário"
+        />
+
+        <div>
+          <span>Bem vindo,</span>
+          <strong>Samuel Santos</strong>
+        </div>
+      </Profile>
+
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
+
+
+    </Container>
+  )
+}
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,16 +1,41 @@
-import { Container } from "./styles"
+import { Container, Links, Content } from "./styles"
 import { Button } from "../../components/Button"
+import { Header } from "../../components/Header"
+import { Section } from "../../components/Section"
+import { ButtonText } from "../../components/ButtonText"
+import { Tag } from "../../components/Tag"
 
 export function Details() {
 
   return (
     <Container>
-      <h1>Hello World!</h1>
-      <p>Samuel P. Santos</p>
+      <Header />
 
-      <Button name="Entrar" loading/>
-      <Button name="Cadastrar"/>
-      <Button name="Voltar"/>
+      <main>
+        <Content>
+
+          <ButtonText title="Excluir nota" />
+
+          <h1>Introdução ao React</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, dicta quos. Tenetur enim officiis sed, dolore iusto, 
+            architecto aliquam voluptatem sapiente corporis impedit itaque expedita minus iste adipisci deserunt similique.</p>
+
+          <Section title="Links Úteis">
+            <Links>
+              <li><a href="#">Link 1</a></li>
+              <li><a href="#">Link 2</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+
+          <Button name="Voltar"/>
+
+        </Content>
+      </main>
     </Container>
   )
 }
